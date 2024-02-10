966de37bb8ee3c009b63610539cb048d7987f2ec
Wesley <wesley.santos140300@gmail.com>
2024-02-10 17:11:20 -0300

    feat: upload project

---
+++ src/app.tsx
@@ -0,0 +1,36 @@
+
+import Logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+const note = {
+  date: new Date(),
+  content: "New World"
+}
+
+export function App() {
+  return (
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={Logo} alt="NWL EXPERT" />
+      
+      <form className='w-full'>
+        <input 
+          type="text" 
+          placeholder='Busque em suas notas...'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+        />
+      </form>
+
+      <div className='h-px bg-slate-700'></div>
+
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+        
+        <NewNoteCard />
+        <NoteCard note={note} />
+        <NoteCard note={note} />
+
+      </div>
+    </div>
+  )
+}
+
